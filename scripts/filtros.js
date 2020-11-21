9ccec0087c78bff5b5f5770a4fbc9a6b95f5dd00
David Leal Pelaez <davidlealpelaez@gmail.com>
2020-11-21 15:05:41 -0500

    Listo el evento onChange para capturar los valores de los select, perndienta ya los fisltros con las fechas combinar todo y sale

--- scripts/filtros.js
+++ scripts/filtros.js
@@ -1,42 +1,60 @@
+const countrys = ["Todos los paises","Argentina","Brasil", "Chile","Uruguay","Mexico"];
+const prices = ["Todos los precios","$","$$","$$$","$$$$"];
+const sizes = ["Todos los tamaños", "Pequeño", "Mediano","Grande"];
+
 class Filtros extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            hotelsData,
-            countrys:["Todos los paises","Argentina","Brasil", "Chile","Uruguay","Mexico"],
-            prices:["Todos los precios","$","$$","$$$","$$$$"],
-            sizes:["Todos los tamaños", "Pequeño", "Mediano","Grande"]
-        }  
-                           
-    }   
-    render(){
-        return(
-            <div className="form row filtro">
-                <div className="form-group col-sm-3">
+        this.state = {country: "Todos los paises"}
+        this.state = {price : "Todos los precios"}
+        this.state = {size : "Todos los tamaños"}
+        
+    
+        this.handleChangeCountry = this.handleChangeCountry.bind(this);
+        this.handleChangePrice = this.handleChangePrice.bind(this);
+        this.handleChangeSize = this.handleChangeSize.bind(this);
+      }
+    
+      handleChangeCountry(event) {
+        this.setState({country: event.target.value});
+        }
+      handleChangePrice(event) {
+        this.setState({price: event.target.value});
+        }
+      handleChangeSize(event) {
+        this.setState({size: event.target.value});
+        }
+    
+     
+      render() {
+        return (
+          <div className="form row filtro">
+              <div className="form-group col-sm-3">
                     <input className="form-control input-field" type="date" name="infentrada" />
                 </div>
                 <div className="form-group col-sm-3">
                     <input className="form-control input-field" type="date" name="infsalida" />
                 </div>
-                <div className="form-group col-sm-2">
-                    <select className="form-control" >
-                        {this.state.countrys.map((country,index)=> 
-                        {return(<option key={index} value={country}>{country}</option>)})}
-                    </select>
-                </div>
-                <div className="form-group col-sm-2">
-                    <select className="form-control">
-                        {this.state.prices.map((price,index)=>
-                        {return(<option key={index} value={price}>{price}</option>)})}
-                    </select>
-                </div>
-                <div className="form-group col-sm-2">
-                    <select className="form-control">
-                        {this.state.sizes.map((size,index)=>
-                        {return(<option key={index} value={size}>{size}</option>)})}
-                    </select>
-                </div>
-            </div>            
+            <div className="form-group col-sm-2">
+                <select className="form-control" value={this.state.country} onChange={this.handleChangeCountry} >
+                    {countrys.map((country,index)=> 
+                    {return(<option key={index} value={country}>{country}</option>)})}
+                </select>
+            </div>
+            <div className="form-group col-sm-2">
+                <select className="form-control" value={this.state.price} onChange={this.handleChangePrice}>
+                    {prices.map((price,index)=>
+                    {return(<option key={index} value={price}>{price}</option>)})}
+                </select>
+            </div>
+            <div className="form-group col-sm-2">
+                <select className="form-control" value={this.state.size} onChange={this.handleChangeSize}>
+                    {sizes.map((size,index)=>
+                    {return(<option key={index} value={size}>{size}</option>)})}
+                </select>
+            </div>
+            
+          </div>
         );
-    }
+      }
 }
